2530ea317e6f3aa618363b7d2f6412353e1d276a
Christo Meiring <32700954+CWMeiring@users.noreply.github.com>
2017-12-06 14:16:36 +0200

    new

---
+++ web.js
@@ -0,0 +1,138 @@
+function one(){
+	var amoverone =
+document.getElementById("pAbout");
+amoverone.style.fontSize = "120%";
+}
+
+function two(){
+	var amoveout =
+document.getElementById("pAbout");
+amoveout.style.fontSize = "100%";	
+}
+
+function onep(){
+	var amovertwo =
+document.getElementById("pcode");
+amovertwo.style.fontSize = "120%";
+}
+
+function twop(){
+	var amoveouttwo =
+document.getElementById("pcode");
+amoveouttwo.style.fontSize = "100%";	
+}
+
+function threep(){
+	var amovepBlog =
+document.getElementById("pBlog");
+amovepBlog.style.fontSize = "120%";
+}
+
+function fourp(){
+	var amovepBlogtwo =
+document.getElementById("pBlog");
+amovepBlogtwo.style.fontSize = "100%";
+}
+
+function fivep(){
+	var amovepFollow =
+document.getElementById("pFollow");
+amovepFollow.style.fontSize = "120%";
+}
+
+function sixp(){
+	var amovepFollowtwo =
+document.getElementById("pFollow");
+amovepFollowtwo.style.fontSize = "100%";
+}
+
+function aboutOn(){
+	var artabouton =
+document.getElementById("artAbout");
+artabouton.style.display = "block";
+	var artcodeOff =
+document.getElementById("artCode");
+artcodeOff.style.display = "none";
+	var artblogOff =
+document.getElementById("artblog");
+artblogOff.style.display = "none";
+	var followon =
+document.getElementById("artFollw");
+followon.style.display = "none";	
+}
+
+function codeOn(){
+	var codeon =
+document.getElementById("artCode");
+codeon.style.display = "block";
+	var artaboutOff =
+document.getElementById("artAbout");
+artaboutOff.style.display = "none";
+	var artblogOff =
+document.getElementById("artblog");
+artblogOff.style.display = "none";
+	var followon =
+document.getElementById("artFollw");
+followon.style.display = "none";
+}
+
+function blogOn(){
+	var blogon =
+document.getElementById("artblog");
+blogon.style.display ="block";
+	var artaboutOff =
+document.getElementById("artAbout");
+artaboutOff.style.display = "none";
+	var artcodeOff =
+document.getElementById("artCode");
+artcodeOff.style.display = "none";
+	var followon =
+document.getElementById("artFollw");
+followon.style.display = "none";
+}
+
+function followOn(){
+	var followon =
+document.getElementById("artFollw");
+followon.style.display = "block";
+	var artaboutOff =
+document.getElementById("artAbout");
+artaboutOff.style.display = "none";
+	var artcodeOff =
+document.getElementById("artCode");
+artcodeOff.style.display = "none";
+	var artblogOff =
+document.getElementById("artblog");
+artblogOff.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
